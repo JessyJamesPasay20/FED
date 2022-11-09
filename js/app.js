01506fbc3fb6030bc2da77fc7238869d523be406
JessyJamesPasay20 <93660089+JessyJamesPasay20@users.noreply.github.com>
2022-11-09 05:14:44 +0100

    V2.0
    Bunch of changes.
    
    Didnt manage to do everything as desired, though Im happy.

--- js/app.js
+++ js/app.js
@@ -1,34 +1,7 @@
-var slider = document.querySelector('section:nth-of-type(4)'); 
-var innerSlider = document.querySelector('section:nth-of-type(4) > ul')
+const hamburgerMenu = document.querySelector('header > button');
+const headerNav = document.querySelector('body > header');
 
-var pressed = false;
-var startSlide;
-var slide;
-
-slider.addEventListener('mmousedown', (e)=> {
-pressed = true;
-startSlide = e.offsetX - innerSlider.offsetLeft;
-console.log(startSlide);
+hamburgerMenu.addEventListener('click', function () {
+    headerNav.classList.toggle("toggled");
 });
 
-slider.addEventListener('mousmove', (e)=> {
-    if(!pressed) return;
-    e.preventDefault();
-
-    slide = e.offsetX
-
-    innerSlider.style.left = '${slide - startSlide}px';
-});
-
-function checkboundary(){
-    let outer = slider.getBoundingClientRect();
-    let inner = innerSlider.getBoundingClientRect();
-
-    if(parseInt(innerSlider.style.left) > 0) {
-        innerSlider.style.left = '0px';
-    }
-    else if (inner.right < outer.right) {
-        innerSlider.style.left = '-${inner.width - outer.right}px';
-
-    }
-}
